9e0b769cee82e23315272f04237f1f8b30078ae9
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-06-01 23:22:52 +0100

    commit tree 2c609f447206abc9b5c43f0d900d9e689f829b0e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+function map(arr, fn) {
+ let r = []
+ for (let i = 0; i < arr.length; i++) {
+  r.push(fn(arr[i]))
+ }
+ return r;
+}
+
+
+function reduce(arr, fn, starting) {
+    let i;
+    if(!starting) {
+      starting = arr[0];
+     i=1;
+    }
+    else{
+        i=0;
+    }
+    let r=starting;
+  
+    for (i; i < arr.length; i++) {
+      r = fn(arr[i], r)
+    }
+  
+    return r;
+  }
+
